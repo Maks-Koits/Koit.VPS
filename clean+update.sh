4ec7140c6995e70946584609141e56aad9eec04e
Maks-Koits <81523737+Maks-Koits@users.noreply.github.com>
2023-02-16 11:23:01 +0300

    v 1.0.0

---
+++ clean+update.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Standard update and clear
+# Removes old revisions of snaps
+# CLOSE ALL SNAPS BEFORE RUNNING THIS
+echo "-------------------------------------"
+echo "------poluchenie spiska paketov------"
+echo "-------------------------------------"
+apt -y update
+echo "-------------------------------------"
+echo "----------ustanovka paketov----------"
+echo "-------------------------------------"
+apt -y upgrade
+echo "-------------------------------------"
+echo "----ochistka dublirovannyh paketov---"
+echo "-------------------------------------"
+set -eu
+snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
+echo "-------------------------------------"
+echo "--------udaleniye stari yader--------"
+echo "-------------------------------------"
+sudo apt-get purge $(dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | head -n -1) -y
+echo "-------------------------------------"
+echo "-------udalenie starih paketov-------"
+echo "-------------------------------------"
+apt -y autoremove
+update-grub
+echo "-------------------------------------"
+echo "----------------done!----------------"
+echo "-------------------------------------"
