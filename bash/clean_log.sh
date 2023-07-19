d63a287ef9f0582ffa273f614a21cda076dabad0
Maks-koits <mix256@gmail.com>
2023-07-19 12:35:18 +0300

    many changes of nginx conf, clean nginx log,compose

--- bash/clean_log.sh
+++ bash/clean_log.sh
@@ -3,9 +3,10 @@
 LOG_ACCESS="/var/log/nginx/access.log"
 LOG_NGINX_CLEAN="/var/log/nginx/nginx_clean.log"
 
-#while  true
-#do
+while  true
+do
 grep -v "node-exporter.maks-koits.click" "$LOG_ACCESS" | grep -v "portainer.maks-koits.click" > "$LOG_NGINX_CLEAN"
 cat "$LOG_NGINX_CLEAN" > "$LOG_ACCESS"
-#        sleep 5
-#done
+rm "$LOG_NGINX_CLEAN"
+        sleep 3600
+done & disown
