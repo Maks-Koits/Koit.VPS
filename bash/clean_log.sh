63148525714aac2c971e1d6d8dabec6dfd5d71a7
Maks-koits <mix256@gmail.com>
2023-09-19 11:34:05 +0300

    edit cron

--- bash/clean_log.sh
+++ bash/clean_log.sh
@@ -3,10 +3,10 @@
 LOG_ACCESS="/var/log/nginx/access.log"
 LOG_NGINX_CLEAN="/var/log/nginx/nginx_clean.log"
 
-while  true
-do
+#while  true
+#do
 grep -v "node-exporter.maks-koits.click" "$LOG_ACCESS" | grep -v "portainer.maks-koits.click" > "$LOG_NGINX_CLEAN"
 cat "$LOG_NGINX_CLEAN" > "$LOG_ACCESS"
 rm "$LOG_NGINX_CLEAN"
-        sleep 3600
-done & disown
+#        sleep 3600
+#done & disown
