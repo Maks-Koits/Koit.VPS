0ca712b79883cb416f3e1137d9b3bd85f5bdf6e2
Maks-Koits <81523737+Maks-Koits@users.noreply.github.com>
2023-12-24 19:04:32 +0300

    changed domain-name maks-koits.

--- bash/clean_log.sh
+++ bash/clean_log.sh
@@ -5,7 +5,7 @@ LOG_NGINX_CLEAN="/var/log/nginx/nginx_clean.log"
 
 #while  true
 #do
-grep -v "node-exporter.maks-koits.click" "$LOG_ACCESS" | grep -v "portainer.maks-koits.click" > "$LOG_NGINX_CLEAN"
+grep -v "node-exporter.maks-koits.space" "$LOG_ACCESS" | grep -v "portainer.maks-koits.space" > "$LOG_NGINX_CLEAN"
 cat "$LOG_NGINX_CLEAN" > "$LOG_ACCESS"
 rm "$LOG_NGINX_CLEAN"
 #        sleep 3600
